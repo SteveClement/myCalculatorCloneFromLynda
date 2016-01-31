aa1090271cdbb89026284a220448f28d1a85e719
Steve Clement <steve@localhost.lu>
2016-01-31 18:53:06 +0100

    - converted labestring to Int

--- myCalculatorCloneFromLynda/ViewController.swift
+++ myCalculatorCloneFromLynda/ViewController.swift
@@ -51,7 +51,12 @@ class ViewController: UIViewController {
   }
   
   func updateText() {
-    label.text = labelString
+    guard let labelInt:Int = Int(labelString) else {
+      label.text = "Int conversion failed"
+      return
+    }
+    
+    label.text = "\(labelInt)"
   }
   
   func changeMode(newMode:modes) {
